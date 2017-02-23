fd7cabacffd445002c0d5be3e14a6abebd10861a
Yu-Ting, Kao @ APU <jackzzjack@gmail.com>
2017-02-24 03:02:19 +0800

    Add diff echo and printf comparion.

---
+++ test-printf.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <semaphore.h>
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define SEM_NAME "/yutingkao"
+
+sem_t *sem;
+
+void getGlobalSemaphoreFromSharedMemory() {
+	sem = sem_open(SEM_NAME, O_CREAT, 0644, 1);
+}
+
+int main() {
+	getGlobalSemaphoreFromSharedMemory();
+
+	sem_wait(sem);
+	
+	int i=0;
+	for (i=0; i < 100000000; i++)
+		printf("Wait ...\n");
+
+	sem_post(sem);
+
+	int val;
+
+	sem_getvalue(sem, &val);
+	printf("Semaphore value is %d\n", val);
+
+	return 0;
+}
